8dfa5e546692a6afa4a054cf691a60e9af6fb3bc
srihari vasupalli <vasupallisrihari@gmail.com>
2022-03-11 00:06:50 +0530

    added form for new expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -24,6 +25,7 @@ function App() {
   ];
   return (
     <div>
+      <NewExpense/>
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
